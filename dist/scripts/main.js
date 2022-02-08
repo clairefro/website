92614fe573d389187457174d62ada3755d9a9ae1
clairefro <claire.froelich@gmail.com>
2022-02-08 13:04:41 -0500

    Use CSS instead of JS for a11y

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,23 +1,3 @@
-// prevent focus box outline from appearing on clicked links (but keep for keyboard accessibilty)
-// grab closest anchor tag to bubble up from non-text links (like svg icon links)
-
-function handleDown(e) {
-  e.target.closest('a').classList.add('mouse');
-}
-
-function handleBlur(e) {
-  e.target.closest('a').classList.remove('mouse');
-  // handle edge case of opening new tab
-  document.activeElement.blur();
-}
-
-const links = document.querySelectorAll('a');
-
-links.forEach((a) => {
-  a.addEventListener('mousedown', handleDown);
-  a.addEventListener('blur', handleBlur);
-});
-
 // Add copyright date in footer
 const copyright = document.getElementById('copyright-goes-here');
 copyright.innerText = `© ${new Date().getFullYear()}`;
